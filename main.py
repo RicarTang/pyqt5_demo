4a2909b5bbfae420cf55310e980ad9a4553c7289
Tang <357965604@qq.com>
2023-02-12 10:57:57 +0800

    添加description输入框的逻辑，修改窗口为Qmainwindow窗口，添加菜单栏

--- main.py
+++ main.py
@@ -1,61 +1,98 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QPushButton, QLabel,QLineEdit,QTextEdit,QFrame,QSplitter,
-    QHBoxLayout, QVBoxLayout, QApplication)
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLabel, QLineEdit, QTextEdit, QFrame, QSplitter,
+                             QHBoxLayout, QVBoxLayout, QApplication, QRadioButton, QMainWindow, QAction)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 import json
 
 
-class Tool(QWidget):
+class Tool(QMainWindow):
+
     def __init__(self):
         # 调用父类构造函数
         super().__init__()
+        # 中心窗口
+        self.widget = QWidget()  # 实例化中心窗口
+        self.setCentralWidget(self.widget)  # 设置中心窗口
+        # 菜单
+        self.menubar = self.menuBar()  # 实例化菜单栏对象
+        self.file_menu = self.menubar.addMenu('File')  # 添加文件菜单
+        self.tip_act = QAction('啥都没有', self)  # 实例化菜单选项
+        self.file_menu.addAction(self.tip_act)  # 添加选项到菜单栏
+        # 计数器
+        self.counter = 0
+        self.local_var = locals()  # 动态控制变量
+        # 全局layout
+        self.wlayout = QVBoxLayout()
+        # 局部layout
+        self.hlayout1 = QHBoxLayout()  # android
+        self.hlayout2 = QHBoxLayout()  # verion
+        self.hlayout3 = QHBoxLayout()  # description
+        self.hlayout4 = QHBoxLayout()  # header
+        self.hlayout5 = QHBoxLayout()  # title
+        self.hlayout6 = QHBoxLayout()  # force
+        self.hlayout7 = QHBoxLayout()  # button start
+        self.vlayout1 = QVBoxLayout()
+        self.vlayout2 = QVBoxLayout()
+        self.vlayout3 = QVBoxLayout()  # description lineEdit控件布局
+        self.widget.setLayout(self.wlayout)  # 添加全局layout到中心窗口
+        # 动态控件layout
+        self.box_layout = QVBoxLayout()
         # 设置控件
-        self.set_control()
-        # 设置布局
-        self.set_layout()
-        # button监听事件，信号插槽
-        self.button_start.clicked.connect(self.show_text)  # 点击开始
-        self.button_add.clicked.connect(self.add_widget)  # 动态添加控件
-        
-        # resize与move方法的整合方法，窗口大小与鼠标放大缩小/移动
-        self.setGeometry(300,300,1000,500)
-        self.setWindowTitle("Tool")
-        # 设置窗口图标
-        self.setWindowIcon(QIcon("./tool.jpg"))
-        # 显示窗口
-        self.show()
-    
-    def set_control(self):
-        """设置窗口控件"""
-        # button_start
-        self.button_start = QPushButton()
-        # button_add
-        self.button_add = QPushButton()
-        # textEdit
-        self.text_edit = QTextEdit()
-        # lable
         self.lable_android = QLabel(self)
         self.lable_version = QLabel(self)
         self.lable_desription = QLabel(self)
         self.lable_header = QLabel(self)
         self.lable_title = QLabel(self)
         self.lable_force = QLabel(self)
-        self.lable_android.setText("android")
-        self.lable_version.setText("version")
-        self.lable_desription.setText("desription")
-        self.lable_header.setText("header")
-        self.lable_title.setText("title")
-        self.lable_force.setText("force")
+        # textEdit
+        self.text_edit = QTextEdit()
+        # button_add
+        self.button_add = QPushButton("add")
+        # button_start
+        self.button_start = QPushButton("start")
         # lineEdit
         self.line_android = QLineEdit()
         self.line_version = QLineEdit()
         self.line_desription = QLineEdit()
         self.line_header = QLineEdit()
         self.line_title = QLineEdit()
-        self.line_force = QLineEdit()
-        self.button_start = QPushButton("start")
+        # radio
+        self.force_radio_t = QRadioButton("true")
+        # 设置force_radio_t为默认选中
+        self.force_radio_t.setChecked(True)
+        self.force_radio_f = QRadioButton("false")
+        # 文本显示
         self.text = QTextEdit()
+        # 控件
+        self.set_control()
+        # 设置布局
+        self.set_layout()
+        # 调同时间监听
+        self.set_event()
+
+        # resize与move方法的整合方法，窗口大小与鼠标放大缩小/移动
+        self.setGeometry(300, 300, 1000, 500)
+        self.setWindowTitle("Tool")
+        # 设置窗口图标
+        self.setWindowIcon(QIcon("./tool.jpg"))
+        # 显示窗口
+        self.show()
+
+    def set_event(self):
+        # button监听事件，信号插槽
+        self.button_start.clicked.connect(self.show_text)  # 点击开始
+        self.button_add.clicked.connect(self.add_widget)  # 动态添加控件
+
+    def set_control(self):
+        """设置窗口控件属性"""
+        # lable
+        self.lable_android.setText("android")
+        self.lable_version.setText("version")
+        self.lable_desription.setText("desription")
+        self.lable_header.setText("header")
+        self.lable_title.setText("title")
+        self.lable_force.setText("force")
 
     def set_layout(self):
         """设置窗口布局"""
@@ -74,25 +111,12 @@ class Tool(QWidget):
         splitter1 = QSplitter(Qt.Horizontal)
         splitter1.addWidget(topleft)
         splitter1.addWidget(self.text_edit)
-        splitter1.setSizes([200,100])  # 设置两边frame比例大小
-        
+        splitter1.setSizes([200, 100])  # 设置两边frame比例大小
+
         splitter2 = QSplitter(Qt.Vertical)
         splitter2.addWidget(splitter1)
         splitter2.addWidget(bottom)
-        splitter2.setSizes([400,100])
-        
-        # 全局
-        self.wlayout = QVBoxLayout(self)
-        # 局部
-        self.hlayout1 = QHBoxLayout()  
-        self.hlayout2 = QHBoxLayout()  
-        self.hlayout3 = QHBoxLayout()  
-        self.hlayout4 = QHBoxLayout()  
-        self.hlayout5 = QHBoxLayout()  
-        self.hlayout6 = QHBoxLayout()  
-        self.hlayout7 = QHBoxLayout()  
-        self.vlayout1 = QVBoxLayout()
-        self.vlayout2 = QVBoxLayout()
+        splitter2.setSizes([400, 100])
 
         # 添加控件
         # android
@@ -103,7 +127,8 @@ class Tool(QWidget):
         self.hlayout2.addWidget(self.line_version)
         # desription
         self.hlayout3.addWidget(self.lable_desription)
-        self.hlayout3.addWidget(self.line_desription)
+        self.hlayout3.addLayout(self.vlayout3)
+        self.vlayout3.addWidget(self.line_desription)
         # header
         self.hlayout4.addWidget(self.lable_header)
         self.hlayout4.addWidget(self.line_header)
@@ -112,9 +137,12 @@ class Tool(QWidget):
         self.hlayout5.addWidget(self.line_title)
         # force
         self.hlayout6.addWidget(self.lable_force)
-        self.hlayout6.addWidget(self.line_force)
+        self.hlayout6.addWidget(self.force_radio_t)
+        self.hlayout6.addWidget(self.force_radio_f)
         # button_start
         self.hlayout7.addWidget(self.button_start)
+        # button_add
+        self.hlayout3.addWidget(self.button_add)
 
         # 添加布局
         # 标签/输入框垂直布局
@@ -129,38 +157,46 @@ class Tool(QWidget):
         topleft.setLayout(self.vlayout1)
         # 添加分隔
         self.wlayout.addWidget(splitter2)
-        # 窗口设置布局
-        self.setLayout(self.wlayout)
-        
-    def description_decide(self):
-        """判断有几个lineEdit,循环拿取内容,return一个list"""
-        pass
-        
-    def show_text(self):
-        """textEdit控件显示文本"""
-        res = { 
-               "android": f"{self.line_android.text()}",  
-                "version": f"{self.line_version.text()}",
-                "description": 
-                     ["1.新增交易详情页，展示历史交易数据;",  
-                       "2.新增BFMeta/BFChain/CCChain等链的非主权益币种;",
-                           "3.修复转账时ETH等链手续费显示错误的BUG."
-                           ],
-                "header": f"{self.line_header.text()}",     
-                "title": f"{self.line_title.text()}",    
-                "force": f"{self.line_force.text()}"
-                }
-        # setPlainText()方法显示文本
-        self.text_edit.setPlainText(json.dumps(
-            res,sort_keys=True,
-            indent=4,
-            ensure_ascii=False))
+        # # 窗口设置布局
+        # self.setLayout(self.wlayout)
+
+    def description_decide(self) -> list:
+        """循环筛选变量名,return一个list"""
+        text_list = []
+        for k, v in self.local_var.items():
+            if k.startswith("line_description"):
+                text_list.append(v.text())
+        return text_list
+
     def add_widget(self):
         """添加控件"""
-        pass
+        self.counter = self.counter + 1
+        self.local_var[f"line_description_{self.counter}"] = QLineEdit()
+        self.vlayout3.addWidget(self.local_var[f"line_description_{self.counter}"])
 
-    
+    def radio_check(self):
+        if self.force_radio_t.isChecked():
+            return True
+        return False
 
+    def show_text(self):
+        """textEdit控件显示文本"""
+        description = self.description_decide()
+        description.insert(0, self.line_desription.text())
+
+        res = {
+            "android": f"{self.line_android.text()}",
+            "version": f"{self.line_version.text()}",
+            "description": description,
+            "header": f"{self.line_header.text()}",
+            "title": f"{self.line_title.text()}",
+            "force": self.radio_check()
+        }
+        # setPlainText()方法显示文本
+        self.text_edit.setPlainText(json.dumps(
+            res,
+            indent=4,
+            ensure_ascii=False))
 
 
 if __name__ == "__main__":
